8f09f8dd66cbe96ff8831d58098ea3f987e81c56
Book-on <s40409841@gmail.com>
2026-09-13 01:22:23 +0900

    feat: [#7] 공통 색상 토큰 추가

---
+++ heatguard-iOS/HGColor.swift
@@ -0,0 +1,17 @@
+//
+//  HGColor.swift
+//  heatguard-iOS
+//
+
+import SwiftUI
+
+enum HGColor {
+    static let appBackground = Color(red: 246 / 255, green: 249 / 255, blue: 252 / 255)
+    static let surface = Color.white
+    static let fieldBackground = Color(red: 247 / 255, green: 250 / 255, blue: 255 / 255)
+    static let metricBackground = Color(red: 243 / 255, green: 244 / 255, blue: 246 / 255)
+    static let primary = Color(red: 46 / 255, green: 128 / 255, blue: 237 / 255)
+    static let primaryText = Color(red: 31 / 255, green: 38 / 255, blue: 51 / 255)
+    static let secondaryText = Color(red: 120 / 255, green: 130 / 255, blue: 148 / 255)
+    static let error = Color(red: 255 / 255, green: 97 / 255, blue: 114 / 255)
+}
